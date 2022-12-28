68e07b9a0c6029fa60bb512a9eb2f13043935215
osaulenkoUA <oom2212@gmail.com>
2022-12-28 15:01:56 +0200

    added ui categoryies

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
+import {useEffect, useState} from "react";
 
-
+interface ICategory{
+    category:string
+    description?:[string]
+}
 export default function Home() {
-  return (
-    <section>
-          <h2 className={'text-4xl text-green'}>BODY</h2>
-    </section>
-  )
+
+    const [items, setItems] = useState<ICategory[]>([]);
+
+    useEffect(() => {
+        const categories = JSON.parse(localStorage.getItem('categories') || '[]');
+        setItems(categories)
+    }, [])
+
+
+    return (
+        <section>
+            <h2 className={'text-4xl text-green'}>BODY</h2>
+           <div className={'grid grid-cols-3'}>
+               {items.map(c=>(
+                   <div key={c.category}>
+                       <p>{c?.category}</p>
+                       {c?.description?.map(d=><p key={d}>{d}</p>)}
+                   </div>
+               ))}
+           </div>
+        </section>
+    )
 }
