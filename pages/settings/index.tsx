8668e7f23f96a913b1b836f17ae7674047a17a41
osaulenkoUA <oom2212@gmail.com>
2022-12-30 12:35:43 +0200

    refresh categories

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,17 +1,31 @@
 import {useState} from "react";
 import Addcategory from "../../components/AddCategory/addcategory";
 import UpdateCategory from "../../components/UpdateCategory/updateCategory";
+import axios from "axios";
+import {fetchCategories} from "../../components/helpers/endpoints";
 
 
 export default function Settings() {
 
     const [activeTab, setActiveTab] = useState('0');
 
+    const getCategories = async () => {
+        try {
+            const {data} = await axios.get(fetchCategories)
+            localStorage.setItem('categories', JSON.stringify(data));
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+
     return (
         <section className={'p-[16px]'}>
-            <div className={'flex gap-4 mb-[20px]'}>
-                <button onClick={() => setActiveTab('0')}>ADD category</button>
-                <button onClick={() => setActiveTab('1')}>Update category</button>
+            <div className={'flex gap-4 mb-[20px] border-b-2 pb-1'}>
+                <button onClick={() => setActiveTab('0')}>Добавити категорію</button>
+                <button onClick={() => setActiveTab('1')}>Добавити опис</button>
+                <button onClick={() => getCategories() }>Оновити список категорій</button>
+
             </div>
             {activeTab === '0' && <Addcategory/>}
             {activeTab === '1' && <UpdateCategory/>}
