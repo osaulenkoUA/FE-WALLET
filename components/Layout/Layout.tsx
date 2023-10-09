90e135013c9c152ae069d5f3f5fbfea980f5d727
Oleksandr_Osaulenko <oosaulen@PMINTL.NET>
2023-10-09 17:42:41 +0300

    removed use-breakpoints-width

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,11 +1,4 @@
-// import breakPoints from '../helpers/breakPoints';
-import {Header} from "../Header/Header";
-
 export function Layout({children}:{children:any}) {
-
-    // const screen = breakPoints();
-    // const isMobile = screen === 'desktop';
-
     return (
             <main >
                 {children}
