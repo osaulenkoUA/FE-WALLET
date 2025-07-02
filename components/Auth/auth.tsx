47fc5afd59805e7ea9ba3e21f61ac03536d4af7e
osaulenkoUa <oom2212@gmail.com>
2025-07-02 18:04:25 +0300

    fixed code

--- components/Auth/auth.tsx
+++ components/Auth/auth.tsx
@@ -1,13 +1,10 @@
 import GoogleAuthButton from "../GoogleAuthButton/GoogleAuthButton";
-import useAuthStore from "../../stores/auth.store";
 
 export function Auth() {
-	const authStore = useAuthStore();
-
 	return (
 		<div>
 			<h2>LogIn with Google</h2>
-			<GoogleAuthButton onHandleClick={authStore.googleAuth} />
+			<GoogleAuthButton  />
 		</div>
 	);
 }
