35286d8bf25dcb172c2b32e9f355884aa746cd76
osaulenkoUa <oom2212@gmail.com>
2026-01-04 20:21:05 +0200

    added 2026 year@2

--- components/analyze-new/Analyze-new.tsx
+++ components/analyze-new/Analyze-new.tsx
@@ -172,9 +172,10 @@ export const AnalyzeNew = () => {
 							<option value="" disabled>
 								Select a Year
 							</option>
-							<option value="2023">2023</option>
 							<option value="2024">2024</option>
 							<option value="2025">2025</option>
+							<option value="2026">2026</option>
+							<option value="2027">2027</option>
 						</select>
 					</div>
 					<div
